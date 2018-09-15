f009501a84b34aad52968563789b2c97d24a5fe3
Thomas Winters <info@thomaswinters.be>
2018-09-15 03:49:23 +0200

    Added combined news retriever

---
+++ src/main/java/be/thomaswinters/newsminer/CombinedNewsRetriever.java
@@ -0,0 +1,52 @@
+package be.thomaswinters.newsminer;
+
+import be.thomaswinters.newsminer.data.ArticleCard;
+import be.thomaswinters.newsminer.data.NewsArticle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class CombinedNewsRetriever implements INewsRetriever {
+    private final List<INewsRetriever> retrievers;
+
+    public CombinedNewsRetriever(List<INewsRetriever> retrievers) {
+        this.retrievers = retrievers;
+    }
+
+    @Override
+    public Collection<ArticleCard> retrieveArticleCards() throws IOException {
+        return retrievers.stream().flatMap(e -> {
+            try {
+                return e.retrieveArticleCards();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public NewsArticle retrieveArticle(URL url) throws IOException {
+        Optional<NewsArticle> newsArticleOptional = retrievers.stream()
+                .filter(e -> e.canRetrieveArticle(url))
+                .map(e -> {
+                    try {
+                        return e.retrieveArticle(url);
+                    } catch (IOException e1) {
+                        throw new RuntimeException("Can't retrieve " + url + " using " + e);
+                    }
+                }).findAny();
+        if (newsArticleOptional.isPresent()) {
+            return newsArticleOptional.get();
+        }
+        throw new RuntimeException("Can't retrieve " + url);
+    }
+
+    @Override
+    public boolean canRetrieveArticle(URL url) {
+        return retrievers.stream().anyMatch(r -> r.canRetrieveArticle(url));
+    }
+}
